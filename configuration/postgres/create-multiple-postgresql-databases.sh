b3c60304d77de873a0d0d6e6c363270457e25dd9
Kasper Helsted <helsted@vopu.dk>
2019-10-05 16:33:30 +0200

    Fixed some shelling

--- configuration/postgres/create-multiple-postgresql-databases.sh
+++ configuration/postgres/create-multiple-postgresql-databases.sh
@@ -6,7 +6,7 @@ set -u
 function create_user_and_database() {
 	local database=$(echo $1 | tr ',' ' ' | awk  '{print $1}')
 	local owner=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
-	echo "  Creating user and database '$database'"
+	echo "  Creating user '$owner' and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $owner IF NOT EXISTS;
 	    CREATE DATABASE $database;
